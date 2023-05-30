8e8b72147ff1df852c6032bd3d868adb391597f2
KroFly03 <krofly03@mail.ru>
2023-05-30 21:36:05 +0300

    Fix docker compose file

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ ENV PYTHONUNBUFFERED 1
 WORKDIR /app
 
 COPY requirements.txt .
+
 RUN python -m pip install --no-cache -r requirements.txt
 
-COPY .. .
+COPY . .
 
 EXPOSE 8000
 
--- todolist/settings.py
+++ todolist/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
 }
 
